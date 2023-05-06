ce02758d5022003f5d912e61694b34a32e4592e1
Kirill Rusinov <kirill.rusinov.04@mail.ru>
2023-05-06 18:18:13 +0300

    Главная страница

---
+++ test3/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public static int Counter = 1;
+
+        public void OnGet()
+        {
+        }
+
+        public PageResult OnPostIncrementCounter()
+        {
+            Counter++;
+            return Page();
+        }
+    }
+}
