8d591d10e7f173423983f63fd9ade3ce853e9227
kirikrus <kirill.rusinov.04@mail.ru>
2023-06-03 13:38:15 +0300

    Прототип конструктора

---
+++ test3/Pages/Constructor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BurgerHUB.Pages
+{
+    public class ConstructorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
